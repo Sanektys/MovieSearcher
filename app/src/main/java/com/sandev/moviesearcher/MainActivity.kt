42ca04054ba10b967e6090622dbc0ba23bdc0986
Ilya Shesterov <Sanektisru@gmail.com>
2023-01-18 00:59:16 +0700

    Central buttons container look has been enhanced
    Also committed few common visual improvements

--- app/src/main/java/com/sandev/moviesearcher/MainActivity.kt
+++ app/src/main/java/com/sandev/moviesearcher/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.sandev.moviesearcher
 
+import android.content.res.Resources
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        menuButtonsInit()
+    }
+
+    private fun menuButtonsInit() {
+        var button: Button = findViewById(R.id.center_container_button1)
+        button.setOnClickListener {
+            Toast.makeText(this,  R.string.activity_main_center_container_button1_text, Toast.LENGTH_SHORT).show()
+        }
+        button = findViewById(R.id.center_container_button2)
+        button.setOnClickListener {
+            Toast.makeText(this, R.string.activity_main_center_container_button2_text, Toast.LENGTH_SHORT).show()
+        }
+        button = findViewById(R.id.center_container_button3)
+        button.setOnClickListener {
+            Toast.makeText(this, R.string.activity_main_center_container_button3_text, Toast.LENGTH_SHORT).show()
+        }
+        button = findViewById(R.id.center_container_button4)
+        button.setOnClickListener {
+            Toast.makeText(this, R.string.activity_main_center_container_button4_text, Toast.LENGTH_SHORT).show()
+        }
+        button = findViewById(R.id.center_container_button5)
+        button.setOnClickListener {
+            Toast.makeText(this, R.string.activity_main_center_container_button5_text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
