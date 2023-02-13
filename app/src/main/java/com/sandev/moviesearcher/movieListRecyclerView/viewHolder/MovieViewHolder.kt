d4a724123c70c1a9f4d79bb204e43524ad79670d
Ilya Shesterov <Sanektisru@gmail.com>
2023-02-13 19:33:29 +0700

    Created movie card with poster and description for recycler list

---
+++ app/src/main/java/com/sandev/moviesearcher/movieListRecyclerView/viewHolder/MovieViewHolder.kt
@@ -0,0 +1,9 @@
+package com.sandev.moviesearcher.movieListRecyclerView.viewHolder
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MovieViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+
+}
